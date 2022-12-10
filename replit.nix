b043a2ee76f50a97a71066077008ae01ceca9d29
gbicyt <108179108+gbicyt@users.noreply.github.com>
2022-12-10 18:42:09 +0700

    Update replit.nix

--- replit.nix
+++ replit.nix
@@ -4,17 +4,7 @@
         pkgs.nodePackages.typescript
         pkgs.ffmpeg
         pkgs.imagemagick
-        pkgs.git
-        pkgs.neofetch
         pkgs.libwebp
-        pkgs.speedtest-cli
-        pkgs.wget
-        pkgs.yarn
-        pkgs.libuuid
+        pkgs.git
     ];
-    env = {
-        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-            pkgs.libuuid
-        ];
-    };
 }
